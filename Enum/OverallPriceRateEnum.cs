27aa127eede74eb6e2913bf113dc8208cea4e4f8
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2021-07-14 01:56:10 +0300

    #204 : Domain classes completed, DB created.

---
+++ Enum/OverallPriceRateEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace DeliveryKing.Core.Enum
+{
+    public enum OverallPriceRateEnum : sbyte
+    {
+        NotSpecified = 0,
+        Cheap = 1,
+        Normal = 2,
+        Expensive = 3
+    }
+}
---
+++ Enum/RoleTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace DeliveryKing.Core.Enum
+{
+    public enum RoleTypeEnum : sbyte
+    {
+            Admin = 1,
+            Vendor = 2,
+            DeliveryPerson = 3,
+            Customer = 4
+    }
+}
