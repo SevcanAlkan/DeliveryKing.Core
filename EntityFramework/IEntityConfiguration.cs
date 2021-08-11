5cca9aff25cab49f1b568a756ec16a040b700b04
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2021-08-11 21:03:05 +0300

    Domain mappings, confgs creating

---
+++ EntityFramework/IEntityConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryKing.Core.EntityFramework
+{
+    public interface IEntityConfiguration
+    {
+        void Configure(ref ModelBuilder builder);
+    }
+}
